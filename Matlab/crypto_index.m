e70c33257b264a443e03407ae7c7723ab2856f19
Christopher Zuber <christopher.zuber@awi.uni-heidelberg.de>
2017-09-25 22:22:37 +0200

    Add code to plot LCI20 at BCH split

--- Matlab/crypto_index.m
+++ Matlab/crypto_index.m
@@ -98,6 +98,46 @@ set(gcf, 'PaperPosition', [0.25 2.5 16.0 8.0]);
 print('-depsc','../bld/figures/lci20.eps')
 
 
+% Plot index evolution during split
+f3 = figure('Name','LCI20 during Split');
+sdate = '2017-07-01';
+edate = '2017-08-31';
+bch_p = [NaN(size(date(date>=sdate&date<='2017-08-01'),1),1); ...
+    df.price(df.symbol=='BCH'&df.date>=sdate&df.date<=edate)];
+[ax,h1,h2] = plotyy(date(date>=sdate&date<=edate), ...
+    df.idx20(df.symbol=='BTC'&df.date>=sdate&df.date<=edate), ...
+    date(date>=sdate&date<=edate), ...
+    [df.price(df.symbol=='BTC'&df.date>=sdate&df.date<=edate)'; bch_p']);
+
+% Formatting commands
+axis 'tight'
+lgnd = legend({'LCI20','Bitcoin Price','Bitcoin Cash Price'},'location','NorthWest','box','off');
+% Change linewidth, color and style of time series
+set(h1,'linewidth', linewdth,'color','k')
+set(h2(1),'linewidth', linewdth,'color','b','LineStyle','--')
+set(h2(2),'linewidth', linewdth,'color','r','LineStyle','--')
+% Format x axes
+set(ax(1),'xcolor','k','ycolor','k','fontsize',fnt_size,'tickdir','out')
+set(ax(2),'xcolor','k', 'ycolor','k','fontsize',fnt_size, ...
+    'tickdir','out','xticklabel',[],'xtick',[])
+linkaxes(ax,'x');
+% Format y axes
+y1sr_lim  = [0, 3000];% Lower and upper bound of y1 axis
+y2sr_lim  = [0, 5500];% Lower and upper bound of y2 axis
+ylim(ax(1),y1sr_lim)
+set(ax(1),'ytick',y1sr_lim(1):500:y1sr_lim(2),'box','off')
+ylim(ax(2),y2sr_lim)
+set(ax(2),'ytick',y2sr_lim(1):500:y2sr_lim(2),'box','off')
+% Manually include top rule
+hold on
+h3 = line(date(date>=sdate&date<=edate),y1sr_lim(2) * ...
+    ones(1,length(date(date>=sdate&date<=edate))));
+set(h3,'linewidth',0.5,'color','k')
+
+% Resize figure and export to eps
+set(gcf, 'PaperPosition', [0.25 2.5 16.0 8.0]);
+print('-depsc','../bld/figures/lci20_bch_split.eps')
+
 
 
 
